d4a18aeab8ff381ea76ae2002002d88351231bff
TrinidadSeg <tsegovialtri@gmail.com>
2026-05-17 14:56:17 -0400

    Add: car-fleet - submission-3

---
+++ Data Structures & Algorithms/car-fleet/submission-3.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int carFleet(int target, vector<int>& position, vector<int>& speed) {
+        vector<pair<int, int>> info;
+        for (int i = 0; i < position.size(); i++) { 
+            info.push_back({target-position[i], speed[i]});
+        }
+        sort(info.begin(), info.end());
+        stack<double> st;
+        for (int i = 0; i < info.size(); i++) {
+            double arrTime = (info[i].first)/(1.0 * info[i].second);
+            if (st.empty()) st.push(arrTime);
+            else if (st.top() < arrTime) st.push(arrTime);
+        }
+        return st.size();
+    }
+};
