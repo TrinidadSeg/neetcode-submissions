06f0d9861a8fd8dc12dce366d9acbb6fd96f2ef2
TrinidadSeg <tsegovialtri@gmail.com>
2026-05-20 22:54:31 -0400

    Add: buy-and-sell-crypto - submission-1

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-1.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int maxSell = 0;
+        int l = 0;
+        int r = 0;
+        while (r < prices.size() - 1) {
+            r++;
+            cout<<l<<" and "<<r<<endl;
+            if (prices[r] < prices[l]) {
+                l = r;
+            }
+            maxSell = max(maxSell, prices[r] - prices[l]);
+        }
+        return maxSell;
+        
+    }
+};
