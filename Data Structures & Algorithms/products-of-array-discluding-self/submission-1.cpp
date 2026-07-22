18ad543a325c62b8717cf3da0a867352cc1d8f51
TrinidadSeg <trinidadasegovia@gmail.com>
2026-07-22 13:03:23 -0400

    Add: products-of-array-discluding-self - submission-1

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-1.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<int> productExceptSelf(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> ans(n, 1);
+        int l = 1;
+        int r = 1;
+        for (int i = 0; i < n; i++) {
+            ans[i] *= l;
+            ans[n-i-1] *= r;
+            l *= nums[i];
+            r *= nums[n-i-1];
+        }
+        return ans;
+
+    }
+};
